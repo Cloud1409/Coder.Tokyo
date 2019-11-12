ff4f07c0aa04fdc2918d0c17e02b722b4f301506
Nguyen_Le <47655411+Cloud1409@users.noreply.github.com>
2019-11-12 19:03:55 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ *  1. Tạo module `circle.js` export 1 constructor function tên là Circle
+ *  2. Circle có 3 property là x, y, radius (toạ độ x, y và bán kính)
+ *  3. Circle có 1 method là isOverlapped (overlap là chồng lấn, đè lên nhau), trả về -1 nếu 2 hình tròn này đè lên nhau, trả về 1 nếu 2 hình tròn không đụng nhau, trả về 0 nếu 2 hình tròn tiếp xúc nhau.
+ *  4. Test thử
+ */
+var Circle = require('./circle.js');
+var circle1 = new Circle(0, 0, 1);
+var circle2 = new Circle(0, 2, 1);
+console.log(circle1.isOverlapped(circle2));
+
+var circle3 = new Circle(0, 0, 3);
+console.log(circle2.isOverlapped(circle3));
+
+var circle4 = new Circle(5, 5, 1);
+console.log(circle1.isOverlapped(circle4));
